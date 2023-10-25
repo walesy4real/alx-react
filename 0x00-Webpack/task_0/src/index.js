0e39fa76b03f0c84530dfb0d9767754406f0f24c
Adewale Olagoke <118176708+walesy4real@users.noreply.github.com>
2023-10-25 07:28:02 +0100

    js file

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,1 +1,5 @@
-add
+const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
